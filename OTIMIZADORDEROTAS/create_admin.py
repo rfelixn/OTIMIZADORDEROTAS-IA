9a90450688b2c92d91fdbc00fa4aa39e258ad2a5
rfelixn <86691530+rfelixn@users.noreply.github.com>
2025-10-24 22:26:23 -0300

    Delete OTIMIZADORDEROTAS directory

--- OTIMIZADORDEROTAS/create_admin.py
+++
@@ -1,9 +0,0 @@
-from app import app
-from models import db, User
-
-with app.app_context():
-    if not User.query.filter_by(username='admin').first():
-        admin = User(username='admin', password='1234', name='Administrador')
-        db.session.add(admin)
-        db.session.commit()
-    print("Admin criado com sucesso!")
